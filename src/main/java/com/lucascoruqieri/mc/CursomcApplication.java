e06328f09dd60395323f46cfc3cb294b4996eaf7
Lucas Coruqieri <lrcoruqieri@gmail.com>
2020-12-22 19:09:38 -0300

    finish

--- src/main/java/com/lucascoruqieri/mc/CursomcApplication.java
+++ src/main/java/com/lucascoruqieri/mc/CursomcApplication.java
@@ -1,18 +1,24 @@
 package com.lucascoruqieri.mc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucascoruqieri.mc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
+
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\840539\\Pictures\\840539.jpg");
 	}
 }
